57f9a14acabbbd40121abcc1d04c15a559f6417c
JurRodi <rodijurrien@gmail.com>
2023-02-23 11:26:20 +0100

    Refactor: remove index & jade engine

--- app.js
+++ app.js
@@ -4,22 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -33,9 +26,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // error massage
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ message: 'error' });
 });
 
 module.exports = app;
