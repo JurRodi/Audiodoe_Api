e4e620c887b0811641aa3bad68f6433e4500c332
JurRodi <rodijurrien@gmail.com>
2023-03-02 13:24:46 +0100

    Feat: add create route

---
+++ routes/account.js
@@ -0,0 +1,7 @@
+let express = require('express');
+let router = express.Router();
+const accountController = require('../controllers/accountController');
+
+router.post('/create', accountController.createAccount);
+
+module.exports = router;
