3807d340f78eeeed32a260ceffed67ddb347c8b2
JurRodi <rodijurrien@gmail.com>
2023-04-20 11:59:41 +0200

    Feat: add get users

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const Account = require('../models/accountModel')
+const User = require('../models/userModel')
 
 const createAccount = async (req, res) => {
   const { firstname, lastname, email, phone, subscriptionType, accountType } =
@@ -38,7 +39,7 @@ const login = async (req, res) => {
           .status(401)
           .json({ message: 'this combination does not match our records' })
       const token = jwt.sign({ id: account._id }, process.env.SECRET, {
-        expiresIn: 86400,
+        expiresIn: 86400, // expires in 24 hours
       })
       res.status(200).json({ auth: true, token: token })
     })
@@ -47,7 +48,18 @@ const login = async (req, res) => {
   }
 }
 
+const getUsers = async (req, res) => {
+  const { accountId } = req.body
+  try {
+    const users = await User.find({ accountId: accountId })
+    res.status(200).json({ users })
+  } catch (err) {
+    res.status(400).json({ message: 'account does not exist' })
+  }
+}
+
 module.exports = {
   createAccount,
   login,
+  getUsers,
 }
--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,10 @@
 let express = require('express')
 let router = express.Router()
 const accountController = require('../controllers/accountController')
+const auth = require('../middleware/auth')
 
 router.post('/create', accountController.createAccount)
 router.post('/login', accountController.login)
+router.post('/users', auth, accountController.getUsers)
 
 module.exports = router
