cbd4c866025c9b451e47f115957d5cf8d03a363d
JurRodi <rodijurrien@gmail.com>
2023-05-04 11:53:41 +0200

    Fix: export error

--- cors.js
+++ cors.js
@@ -1,6 +1,6 @@
 const whiteList = ['http://localhost:3000', 'https://app.audiodoe.com']
 
-export const corsOptions = {
+const corsOptions = {
   origin: function (origin, callback) {
     if (whiteList.indexOf(origin) !== -1) {
       callback(null, true)
