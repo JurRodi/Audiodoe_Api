8cbbdbe6fc5e66f6a9b4ae92309ce9a9bf872380
JurRodi <rodijurrien@gmail.com>
2023-06-20 08:57:53 +0200

    Feat: add get all pages & update when page already created

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -20,29 +20,59 @@ const create = async (req, res) => {
       storyId,
       interaction,
     } = item
-    const page = new Page({
-      pageNumber,
-      backgroundImage,
-      backgroundColor,
-      animations,
-      choicePath,
-      choiceSplit,
-      choiceQuestion,
-      choices,
-      choiceImage1,
-      choiceImage2,
-      instructionsTitle,
-      audio,
-      text,
-      pageType,
-      storyId,
-      interaction,
+    const page = await Page.findOne({
+      pageNumber: pageNumber,
+      storyId: storyId,
+      choicePath: choicePath,
     })
+    if (!page) {
+      const page = new Page({
+        pageNumber,
+        backgroundImage,
+        backgroundColor,
+        animations,
+        choicePath,
+        choiceSplit,
+        choiceQuestion,
+        choices,
+        choiceImage1,
+        choiceImage2,
+        instructionsTitle,
+        audio,
+        text,
+        pageType,
+        storyId,
+        interaction,
+      })
+      try {
+        await page.save()
+        res.status(201).json({ message: 'page created' })
+      } catch (err) {
+        res.status(400).json({ message: 'page not created' })
+      }
+    }
     try {
+      page.pageNumber = pageNumber
+      page.backgroundImage = backgroundImage
+      page.backgroundColor = backgroundColor
+      page.animations = animations
+      page.choicePath = choicePath
+      page.choiceSplit = choiceSplit
+      page.choiceQuestion = choiceQuestion
+      page.choices = choices
+      page.choiceImage1 = choiceImage1
+      page.choiceImage2 = choiceImage2
+      page.instructionsTitle = instructionsTitle
+      page.audio = audio
+      page.text = text
+      page.pageType = pageType
+      page.storyId = storyId
+      page.interaction = interaction
+      page.updatedAt = Date.now()
       await page.save()
-      res.status(201).json({ message: 'pages created' })
-    } catch (error) {
-      res.status(400).json({ message: 'pages not created', error: error })
+      res.status(200).json({ message: 'page updated' })
+    } catch (err) {
+      res.status(400).json({ message: 'page not updated' })
     }
   })
 }
@@ -61,7 +91,18 @@ const getPage = async (req, res) => {
   }
 }
 
+const getAll = async (req, res) => {
+  const { storyId } = req.params
+  try {
+    const pages = await Page.find({ storyId: storyId })
+    res.status(200).json(pages)
+  } catch (error) {
+    res.status(400).json({ message: 'pages not found', error: error })
+  }
+}
+
 module.exports = {
   create,
   getPage,
+  getAll,
 }
--- routes/page.js
+++ routes/page.js
@@ -5,5 +5,6 @@ const auth = require('../middleware/auth')
 
 router.post('/create', auth, pageController.create)
 router.get('/:storyId/:pageNumber/:choicePath', auth, pageController.getPage)
+router.get('/:storyId', auth, pageController.getAll)
 
 module.exports = router
